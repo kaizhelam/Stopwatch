3b7af545453137c2faa2c4285e311c0d822ea5b9
hayme <jamesmatthewbelgica@gmail.com>
2021-09-11 08:47:00 +0800

    refined script

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 let seconds = 00;
 let tens = 00;
-let OutputSeconds = document.getElementById("second");
-let OutputTens = document.getElementById("tens");
-let buttonStart = document.getElementById("btn-start");
-let buttonStop = document.getElementById("btn-stop");
-let buttonReset = document.getElementById("btn-reset");
+let OutputSeconds = document.getElementById('second');
+let OutputTens = document.getElementById('tens');
+let buttonStart = document.getElementById('btn-start');
+let buttonStop = document.getElementById('btn-stop');
+let buttonReset = document.getElementById('btn-reset');
 let Interval;
 
-buttonStart.addEventListener("click", () => {
-  clearInterval(Interval);
-  Interval = setInterval(startTime, 10);
+buttonStart.addEventListener('click', () => {
+    clearInterval(Interval);
+    Interval = setInterval(startTime, 10);
 });
-buttonStop.addEventListener("click", () => clearInterval(Interval));
-buttonReset.addEventListener("click", () => {
-  clearInterval(Interval);
-  seconds = tens = "00";
-  OutputSeconds.innerHTML = seconds;
-  OutputTens.innerHTML = tens;
+buttonStop.addEventListener('click', () => clearInterval(Interval));
+buttonReset.addEventListener('click', () => {
+    clearInterval(Interval);
+    seconds = tens = "00";
+    OutputSeconds.innerHTML = seconds;
+    OutputTens.innerHTML = tens;
 });
 
-function startTime() {
-  tens++;
-  if (tens <= 9) OutputTens.innerHTML = "0" + tens;
-  else OutputTens.innerHTML = tens;
-
-  if (tens > 99) {
-    seconds++;
-    if (seconds > 9) OutputSeconds.innerHTML = seconds;
-    else OutputSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    OutputTens.innerHTML = "0" + tens;
-  }
+function startTime(){
+    tens++;
+    if(tens <= 9)OutputTens.innerHTML = "0" + tens;
+    else OutputTens.innerHTML = tens;
+    
+    if(tens > 99){
+        seconds++;
+        if(seconds > 9)OutputSeconds.innerHTML = seconds;
+        else OutputSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        OutputTens.innerHTML = "0" + tens;
+    }
 }
